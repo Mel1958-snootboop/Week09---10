1f9df917691d5a484cfd4d087d966cd262128359
Melanie <1958Melanie.Castillo@gmail.com>
2025-10-27 10:35:15 -0700

    commit

--- src/lib/firebase/clientApp.js
+++ src/lib/firebase/clientApp.js
@@ -7,7 +7,18 @@ import { getStorage } from "firebase/storage";
 
 // Use automatic initialization
 // https://firebase.google.com/docs/app-hosting/firebase-sdks#initialize-with-no-arguments
-export const firebaseApp = initializeApp();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAku0Q6ZIhi54g3qfqZdRJ8be3mfm6sF3k",
+  authDomain: "week09---10.firebaseapp.com",
+  projectId: "week09---10",
+  storageBucket: "week09---10.firebasestorage.app",
+  messagingSenderId: "597020325861",
+  appId: "1:597020325861:web:635627a75d1f007d60a10c",
+  measurementId: "G-DGE3CE2KRK"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
 
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -17,12 +17,25 @@ import { getAuth } from "firebase/auth";
 export async function getAuthenticatedAppForUser() {
   const authIdToken = (await cookies()).get("__session")?.value;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAku0Q6ZIhi54g3qfqZdRJ8be3mfm6sF3k",
+  authDomain: "week09---10.firebaseapp.com",
+  projectId: "week09---10",
+  storageBucket: "week09---10.firebasestorage.app",
+  messagingSenderId: "597020325861",
+  appId: "1:597020325861:web:635627a75d1f007d60a10c",
+  measurementId: "G-DGE3CE2KRK"
+};
+
+
+
+
   // Firebase Server App is a new feature in the JS SDK that allows you to
   // instantiate the SDK with credentials retrieved from the client & has
   // other affordances for use in server environments.
   const firebaseServerApp = initializeServerApp(
     // https://github.com/firebase/firebase-js-sdk/issues/8863#issuecomment-2751401913
-    initializeApp(),
+    initializeApp(firebaseConfig),
     {
       authIdToken,
     }
